6e522e96961b4b263297a7c2bd5aad3fa5fd5c77
宋志宗 <404869355@qq.com>
2020-12-15 00:47:05 +0800

    mark VisibleException resMsg nullable

--- ideal-bus-abstract/src/main/java/com/zzsong/bus/abs/share/VisibleException.java
+++ ideal-bus-abstract/src/main/java/com/zzsong/bus/abs/share/VisibleException.java
@@ -1,12 +1,14 @@
 package com.zzsong.bus.abs.share;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * @author 宋志宗 on 2020/8/23
  */
 public class VisibleException extends RuntimeException {
 
+  @Nullable
   private ResMsg resMsg = null;
 
   public VisibleException(@Nonnull String message) {
@@ -23,6 +25,7 @@ public class VisibleException extends RuntimeException {
     this.resMsg = resMsg;
   }
 
+  @Nullable
   public ResMsg getResMsg() {
     return resMsg;
   }
