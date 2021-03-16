1a2a479659e7a60ef0fca39d2939348b4afcda5d
宋志宗 <404869355@qq.com>
2021-03-16 14:31:39 +0800

    添加日志配置 / 修改reactor线程数

--- ideal-bus-broker-server/src/main/java/com/zzsong/bus/broker/server/BrokerApplication.java
+++ ideal-bus-broker-server/src/main/java/com/zzsong/bus/broker/server/BrokerApplication.java
@@ -9,10 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = "com.zzsong.bus")
 public class BrokerApplication {
   public static void main(String[] args) {
-    // 将 reactor-http-nio 线程数量设置为 cpu * 4
-//    int availableProcessors = Runtime.getRuntime().availableProcessors();
-//    int ioWorkerCount = availableProcessors << 2;
-//    System.setProperty("reactor.netty.ioWorkerCount", ioWorkerCount + "");
+    // 将 reactor 线程数量设置为 cpu * 4
+    int availableProcessors = Runtime.getRuntime().availableProcessors();
+    int ioWorkerCount = availableProcessors << 2;
+    System.setProperty("reactor.netty.ioWorkerCount", ioWorkerCount + "");
     SpringApplication.run(BrokerApplication.class, args);
   }
 }
